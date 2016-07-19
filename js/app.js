98affefdc8a3db288810a8f631ca779c7e556610
R. Terrado <r.terrado@gmail.com>
2016-07-19 08:54:22 -0700

    Switching drivers

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
-$('#js-shopping-list-form').submit(function)(event) {
-	// event.preventDefault();
-	
-	}
-}
+$('#js-shopping-list-form').submit(function(event) {
+  event.preventDefault();
+  var newItem = ''
+        + '<li>'
+        + '<span class="shopping-item">'
+        + $("#shopping-list-entry").val()
+        + '</span>'
+        + '<div class="shopping-item-controls">'
+        + '<button class="shopping-item-toggle">'
+        + '<span class="button-label">check</span>'
+        + '</button> '
+        + '<button class="shopping-item-delete">'
+        + '<span class="button-label">delete</span>'
+        + '</button>' 
+        + '</div>' 
+        + '</li>';
+	$(newItem).appendTo(".shopping-list");
+  $(this)[0].reset();
+})
+$('.shopping-list').on('click','.shopping-item-delete',function(event){this.closest("li").remove();})
 
+})
