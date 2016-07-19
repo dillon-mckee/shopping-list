55df75a0dd87d7973211fd8993750f8b40459f87
R. Terrado <r.terrado@gmail.com>
2016-07-19 11:04:25 -0700

    Added toggle functionality

--- js/app.js
+++ js/app.js
@@ -25,10 +25,10 @@ $('.shopping-list').on('click','.shopping-item-delete',function(event) {
 $('.shopping-list').on('click','.shopping-item-toggle', function(event) {
     $(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
 });
-$('.shopping-list').on('click','.toggle-checked', function(event) {
-    $(this).find(".shopping-item__checked").toggleClass("item-hide");
+$('body').on('click','button.toggle-checked', function(event) {
+    $('.shopping-list').find(".shopping-item__checked").closest("li").toggleClass("item-hide");
 });
-$('.shopping-list').on('click','.toggle-unchecked', function(event) {
-    $(this).find(".shopping-item").toggleClass("item-hide");
+$('body').on('click','button.toggle-unchecked', function(event) {
+    $('.shopping-list').find("span[class='shopping-item']").closest("li").toggleClass("item-hide");
 });
 });
